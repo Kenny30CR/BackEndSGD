8fd15a84ea9ec50a7511203a63bb714d6871c878
JuanDiegoQ <49623019+JuanDiegoQ@users.noreply.github.com>
2021-06-19 19:05:56 -0600

    Update index.php
    prueba cambio

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+echo"fdds";
 header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-with, Content-Type, Accept, Access-Control-Request-Method, Authorization');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 header('Allow: GET, POST, OPTIONS, PUT, DELETE');
