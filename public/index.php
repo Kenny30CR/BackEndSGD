d712c981d9ce0f6374c02d67d90b25c9e7cf5910
erclpz-hash <57535784+erclpz-hash@users.noreply.github.com>
2021-06-19 19:01:42 -0600

    Update index.php
    Ready

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-echo "Holaaaaa";
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-with, Content-Type, Accept, Access-Control-Request-Method, Authorization');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
