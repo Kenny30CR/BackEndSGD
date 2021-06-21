8dbc0c0c4302994777c359a7b24a0eb65d0ad6ab
Kenny Richards <koneilrb@hotmail.com>
2021-06-20 22:03:24 -0600

    Update Config.php

--- src/App/Config.php
+++ src/App/Config.php
@@ -4,7 +4,7 @@ $container->set('Config_bd',function(){
         "host" => "localhost",
         "bd" => "SGD",
         "usr" =>  "root",
-        "pass" => "root",
+        "pass" => "", //root
         "charset" => "utf8",
         'collation' => 'utf8_spanish_ci',
     ];
